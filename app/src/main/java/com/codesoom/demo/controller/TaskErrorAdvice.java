6d14798564c8e6fa97442c73e57e0c20f19a3ad2
jihwooon <jihwooon@gmail.com>
2022-02-16 22:10:50 +0900

    add : ErrorAdvice 추가
    add ErrorResponse, TaskErrorAdvice

---
+++ app/src/main/java/com/codesoom/demo/controller/TaskErrorAdvice.java
@@ -0,0 +1,18 @@
+package com.codesoom.demo.controller;
+
+import com.codesoom.demo.TaskNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class TaskErrorAdvice {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TaskNotFoundException.class)
+    public String handleNotFound() {
+        return "Not Found";
+    }
+}
