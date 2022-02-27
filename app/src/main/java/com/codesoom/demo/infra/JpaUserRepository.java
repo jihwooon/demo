49025266d5665af0d36171e23e9437697333109d
jihwooon <jihwooon@gmail.com>
2022-02-27 21:55:35 +0900

    test : 회원 컨트롤러에서 create 메서드 mock 테스트
    mock를 사용하여 userSerive.createUser 작동 여부 확인

---
+++ app/src/main/java/com/codesoom/demo/infra/JpaUserRepository.java
@@ -0,0 +1,14 @@
+package com.codesoom.demo.infra;
+
+import com.codesoom.demo.domain.User;
+import com.codesoom.demo.domain.UserRepository;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface JpaUserRepository
+    extends UserRepository, CrudRepository<User, Long>{
+
+    User save(User user);
+
+
+}
