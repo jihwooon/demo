29760dc62400c632858d7a723c580102673b5836
jihwooon <jihwooon@gmail.com>
2022-03-01 01:04:24 +0900

    feat : 회원 컨트롤러에서 create메서드가 202 상태코드를 응답하도록 테스트

--- app/src/main/java/com/codesoom/demo/controller/UserController.java
+++ app/src/main/java/com/codesoom/demo/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.codesoom.demo.controller;
 
+import com.codesoom.demo.dto.UserData;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserData create() {
+        return null;
+    }
+
 }
