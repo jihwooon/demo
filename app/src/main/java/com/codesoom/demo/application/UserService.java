c0c32ad2c8171c4f3f283cefef7850335c9b8a4e
jihwooon <jihwooon@gmail.com>
2022-03-01 22:56:04 +0900

    test : 회원 서비스에서 테스트 생성

--- app/src/main/java/com/codesoom/demo/application/UserService.java
+++ app/src/main/java/com/codesoom/demo/application/UserService.java
@@ -4,7 +4,10 @@ import com.codesoom.demo.domain.User;
 import com.codesoom.demo.dto.UserRegistrationData;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class UserService {
 
     public User registerUser(UserRegistrationData userRegistrationData) {
---
+++ app/src/test/java/com/codesoom/demo/application/UserServiceTest.java
@@ -0,0 +1,7 @@
+package com.codesoom.demo.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+}
