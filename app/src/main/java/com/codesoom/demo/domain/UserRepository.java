ccc0b664a9f2831e324c255e3a3b14206e1749d0
jihwooon <jihwooon@gmail.com>
2022-03-02 00:49:04 +0900

    fix : 회원 서비스 테스트 테스트 문제 해결
    오류 원인
    Caused by: com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `com.codesoom.demo.dto.UserRegistrationData` (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
     at [Source: (PushbackInputStream); line: 1, column: 2]
    
    오류 해결
    UserRegistrationData에 @NoArgsConstructor 어노테이션 추가
    즉 기본 생성자가 있어야 합니다.

---
+++ app/src/main/java/com/codesoom/demo/domain/UserRepository.java
@@ -0,0 +1,5 @@
+package com.codesoom.demo.domain;
+
+public interface UserRepository {
+    User save(User user);
+}
---
+++ app/src/main/java/com/codesoom/demo/infra/JpaUserRepository.java
@@ -0,0 +1,14 @@
+package com.codesoom.demo.infra;
+
+import com.codesoom.demo.domain.User;
+import com.codesoom.demo.domain.UserRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Primary
+public interface JpaUserRepository
+    extends UserRepository, JpaRepository<User, Long> {
+
+    User save(User user);
+
+}
